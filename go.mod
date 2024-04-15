3b776b996f5a6dd89eb7a3d8a81c2d589e12d308
kenyapiberry <Rejie@yandex.ru>
2024-04-16 00:53:59 +0500

    start

---
+++ go.mod
@@ -0,0 +1,3 @@
+module AltTestKataCalc
+
+go 1.22
---
+++ main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func sumStrings(a, b string) string {
+	return a + b
+}
+
+func subtractStrings(a, b string) string {
+	return strings.Replace(a, b, "", 1)
+}
+
+func multiplyStringByNumber(a string, b int) string {
+	result := ""
+	for i := 0; i < b; i++ {
+		result += a
+	}
+	return result
+}
+
+func divideStringByNumber(a string, b int) string {
+	return a[:len(a)/b]
+}
+
+func main() {
+	fmt.Print("Калькулятор умеет выполнять операции сложения строк, \n" +
+		"вычитания строки из строки, умножения строки на число \n" +
+		"и деления строки на число: " +
+		"\n \"a\" + \"b\", \"a\" - \"b\", \"a\" * b, \"a\" / b. " +
+		"\n Значения строк, передаваемых в выражении, выделяются двойными кавычками." +
+		"\n Данные передаются в одну строку: ")
+	reader := bufio.NewReader(os.Stdin)
+
+	input, _ := reader.ReadString('\n')
+
+	input = strings.TrimSuffix(input, "\n")
+
+	//Проверяем, что строка начинается и заканчивается на одинарные кавычки
+	if !strings.HasPrefix(input, "") && !strings.HasSuffix(input, "") {
+		fmt.Println("Выделите двойными кавычками строковые значения")
+		return
+	}
+
+	// Удаляем кавычки из введенной строки
+	//input = input[1 : len(input)-1]
+
+	data := strings.Fields(input)
+
+	//for i, word := range data {
+	//	data[i] = strings.Trim(word, "")
+	//}
+
+	if len(data) != 3 {
+		fmt.Println("Некорректный формат ввода")
+		return
+	}
+
+	// Преобразование чисел и оператора
+	//num, err := strconv.Atoi(data[2])
+	//if err != nil || num < 1 || num > 10 {
+	//	fmt.Println("Пожалуйста, введите число от 1 до 10")
+	//	return
+	//}
+
+	operator := data[1]
+	a := data[0]
+	b := data[2]
+
+	var result string
+
+	switch operator {
+	case "+":
+		result = sumStrings(a, b)
+	case "-":
+		result = subtractStrings(a, b)
+	case "*":
+		num, _ := strconv.Atoi(b)
+		result = multiplyStringByNumber(a, num)
+	case "/":
+		num, _ := strconv.Atoi(b)
+		result = divideStringByNumber(a, num)
+	default:
+		fmt.Println("Некорректный ввод")
+		return
+	}
+
+	fmt.Println("Результат:", result)
+}
